a34b03a7343745352171bbbd090f14a8e37e0f9b
PrzemekGdula <przemo.g12@wp.pl>
2019-03-26 13:45:46 +0100

    fetch data

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const promise = fetch('https://randomuser.me/api') // fetch obiecuje ze dostarczy dane
 
-
 promise
-.then((response) => console.log(response.json())) //response obiecuje, ze wykona funkcje, json obiecuje, ze prztworzy dane
-
-//response.json() rowniez zwraca promise, ale ta promisa resolvuje sie do sparsownych danych, parsed JSON data
+    .then(response => {
+        response.json()
+            .then(data => {
+                console.log(data)
+            })
+    })

39f570b1a4d0909db737ec9ba790f9324c3208e6
PrzemekGdula <przemo.g12@wp.pl>
2019-03-26 14:12:43 +0100

    catching errors -bad url

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-fetch('https://randomuser.me/api')
-.then(response => response.json())
-.then(data => console.log(data))
+fetch('https://randomuser.meeeeee/api')
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.log('Error', error))
 
 //nauczyc sie na pamiec

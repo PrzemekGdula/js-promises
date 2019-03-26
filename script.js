7bdb8b7d29ce2dee4c7f1915de63b578e84713ca
PrzemekGdula <przemo.g12@wp.pl>
2019-03-26 14:14:42 +0100

    do something, then. then, finally

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ fetch('https://randomuser.meeeeee/api')
     .then(response => response.json())
     .then(data => console.log(data))
     .catch(error => console.log('Error', error))
-
+    .finally(() => console.log('I will be here always!'))
 //nauczyc sie na pamiec

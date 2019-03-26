37a074fcf265829908e1ba701728f59f0edae543
PrzemekGdula <przemo.g12@wp.pl>
2019-03-26 14:30:51 +0100

    fetch write in body loading or error occured

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-fetch('https://randomuser.meeeeee/api')
+const writeInBody = message => document.body.innerText = message
+
+writeInBody('Laduje...')
+
+fetch('https://randomuser.me/api')
     .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.log('Error', error))
-    .finally(() => console.log('I will be here always!'))
-//nauczyc sie na pamiec
+    .then(data => {
+        console.log(data.results[0].name)
+        document.body.innerText = (
+            data.results[0].name.first +
+            ' ' +
+            data.results[0].name.last
+        )
+    })
+    .catch(error => writeInBody('Wystapil blad!'))
+

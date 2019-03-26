e727049efce36b41e2b21f6ec21271aed4c6f44b
PrzemekGdula <przemo.g12@wp.pl>
2019-03-26 13:36:07 +0100

    promise response

--- script.js
+++ script.js
@@ -1,22 +1,8 @@
-class NumbersContainer {
-    constructor() {
-        this.numbers = []
-    }
-    addNumber(number) {
-        this.numbers = this.numbers.concat(number)
-        return this
+const promise = fetch('https://randomuser.me/api')
 
-    }
-}
+let response = null
 
-const container1 = new NumbersContainer()
+promise
+.then((promiseValue) => {response = promiseValue})
 
-container1.addNumber(4) // { numbers: [4] }
-container1.addNumber(6) // { numbers: [4, 6] }
-
-container1
-    .addNumber(6)
-    .addNumber(9)
-    .addNumber(11) // { numbers: [4, 7, 6, 9, 11] }
-
-console.log(container1)
+console.log(response) // we cant get promise value here
